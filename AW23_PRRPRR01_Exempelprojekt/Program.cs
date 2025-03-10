81b6fc5f0c537cce22c94c3e35e220d43ee5190b
magisterblaman <hannes.blaman@lbs.se>
2025-03-10 14:06:12 +0100

    Före bensträckaren

--- AW23_PRRPRR01_Exempelprojekt/Program.cs
+++ AW23_PRRPRR01_Exempelprojekt/Program.cs
@@ -27,14 +27,72 @@ namespace AW23_PRRPRR01_Exempelprojekt {
 			}
 		}
 
-		static void AddPiece(int piece, int col) {
+		static int AddPiece(int piece, int col) {
 
 			for (int r = board.GetLength(1) - 1; r >= 0; r--) {
 				if (board[col, r] == 0) {
 					board[col, r] = piece;
-					return;
+					return r;
 				}
 			}
+
+			return -1;
+		}
+
+		static int CheckForWin(int lastPlacedRow, int lastPlacedCol) {
+			int lastPlaced = board[lastPlacedCol, lastPlacedRow];
+
+			int horizontalMatchCounter = 0;
+			for (int i = -1; i >= -3; i--) {
+				if (lastPlacedCol + i < 0) {
+					break;
+				}
+				if (board[lastPlacedCol + i, lastPlacedRow] == lastPlaced) {
+					horizontalMatchCounter++;
+				} else {
+					break;
+				}
+			}
+			for (int i = 1; i <= 3; i++) {
+				if (lastPlacedCol + i >= board.GetLength(0)) {
+					break;
+				}
+				if (board[lastPlacedCol + i, lastPlacedRow] == lastPlaced) {
+					horizontalMatchCounter++;
+				} else {
+					break;
+				}
+			}
+			if (horizontalMatchCounter >= 3) {
+				return lastPlaced;
+			}
+
+			int verticalMatchCounter = 0;
+			for (int i = -1; i >= -3; i--) {
+				if (lastPlacedRow + i < 0) {
+					break;
+				}
+				if (board[lastPlacedCol, lastPlacedRow + i] == lastPlaced) {
+					verticalMatchCounter++;
+				} else {
+					break;
+				}
+			}
+			for (int i = 1; i <= 3; i++) {
+				if (lastPlaced + i >= board.GetLength(1)) {
+					break;
+				}
+				if (board[lastPlacedCol, lastPlacedRow + i] == lastPlaced) {
+					verticalMatchCounter++;
+				} else {
+					break;
+				}
+			}
+			if (verticalMatchCounter >= 3) {
+				return lastPlaced;
+			}
+
+			return 0;
 		}
 
 		static void CommandSystem() {
@@ -49,8 +107,21 @@ namespace AW23_PRRPRR01_Exempelprojekt {
 
 					int column = int.Parse(commandTokens[2]);
 
-					AddPiece(currentPlayer, column);
+					int row = AddPiece(currentPlayer, column);
 
+					if (row == -1) {
+						// Hittade inte
+					}
+
+					int win = CheckForWin(row, column);
+
+					if (win == 1) {
+						Console.WriteLine("X vann");
+						Console.ReadLine();
+					} else if (win == 2) {
+						Console.WriteLine("O vann");
+						Console.ReadLine();
+					}
 				}
 
 			}
